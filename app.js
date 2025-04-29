189a3ef89d9fd946e446e794b8706ffa0d54ab82
Jyri Lindroos <59079476+keudaope@users.noreply.github.com>
2025-04-29 05:31:51 +0300

    video11 lopulliset tiedostot

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
-
+const blogRoutes = require("./routes/blogRoutes");
 // express app
 const app = express();
 
@@ -23,71 +22,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded());
 app.use(morgan("dev"));
 
-// mongoose and mongo sandbox routes
-// app.get("/add-blog", (req, res) => {
-//   const blog = new Blog({
-//     title: "uusi blogi 2",
-//     snippet: "Uudesta blogistani",
-//     body: "lisää uudesta blogistani",
-//   });
-//   blog
-//     .save()
-//     .then((result) => {
-//       res.send(result);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get("/all-blogs", (req, res) => {
-//   Blog.find()
-//     .then((result) => {
-//       res.send(result);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get("/single-blog", (req, res) => {
-//   Blog.findById("680b5c48cf2183e167deb735")
-//     .then((result) => {
-//       res.send(result);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-// app.use((req, res, next) => {
-//   console.log("New request made:");
-//   console.log("Host: ", req.hostname);
-//   console.log("Path: ", req.path);
-//   console.log("Method: ", req.method);
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log("In the next middleware");
-//   next();
-// });
-
 app.get("/", (req, res) => {
-  // const blogs = [
-  //   {
-  //     title: "Yoshi finds eggs",
-  //     snippet: "Lorem ipsum dolor sit amet consectetur",
-  //   },
-  //   {
-  //     title: "Mario finds stars",
-  //     snippet: "Lorem ipsum dolor sit amet consectetur",
-  //   },
-  //   {
-  //     title: "How to defeat browser",
-  //     snippet: "Lorem ipsum dolor sit amet consectetur",
-  //   },
-  // ];
-  // res.render("index", { title: "Home", blogs });
   res.redirect("/blogs");
 });
 
@@ -96,55 +31,7 @@ app.get("/about", (req, res) => {
 });
 
 // blog routes
-
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "New Blog" });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+app.use("/blogs", blogRoutes);
 
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
